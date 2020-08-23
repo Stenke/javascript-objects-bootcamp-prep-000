cd73bccceadd95e50cac8f8ac629108907d0eddc
Stephen Enke <stephenenke@gmail.com>
2020-08-23 18:08:07 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {aristName: "song"}
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, {[artistName]: songTitle})
+  return Object.assign(playlist, {[artistName]: songTitle})
 }
 function removeFromPlaylist(playlist, artistName) {
   
